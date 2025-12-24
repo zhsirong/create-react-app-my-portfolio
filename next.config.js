9a02cfcc0f9ac97ca0a06ba3a6cdcf1557658b19
zhsirong <rosyzhao2001@gmail.com>
2025-12-24 10:18:49 +0800

    Fix basePath for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/create-react-app-my-portfolio",
+  assetPrefix: "/create-react-app-my-portfolio/",
+};
+
+module.exports = nextConfig;
