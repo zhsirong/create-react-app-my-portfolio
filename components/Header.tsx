e4f9a2ebb8b82893a7ad7990af17d5f6016e3210
zhsirong <144848354+zhsirong@users.noreply.github.com>
2025-12-24 10:57:36 +0800

    Update header information for Rosy Zhao

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,11 @@ export default function Header() {
   return (
     <header className="flex justify-between items-start pt-16 pb-20 text-[11px] text-gray-500 uppercase tracking-widest">
       <div>
-        <h1 className="text-white text-lg font-medium normal-case tracking-normal mb-1">Arsen Kolyba</h1>
-        <p>Software designer <br/> Currently at Wix</p>
+        <h1 className="text-white text-lg font-medium normal-case tracking-normal mb-1"Rosy Zhao</h1>
+        <p>Product designer <br/> with business sense & dev skills</p>
       </div>
       <div className="max-w-[300px] text-right normal-case tracking-normal">
-        <p>Researching, prototyping, designing and testing by day; coding, no-coding, launching products by night.</p>
+        <p>Design + business + engineering — from concept to launch.</p>
         <p className="mt-4 text-white">Press <span className="underline italic">m</span> to reveal email</p>
       </div>
     </header>
